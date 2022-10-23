fa863a82495ebcd2f595b5c9d52fe4e9b2f7aad3
salvolannister <darksawyer@hotmail.it>
2022-10-23 08:44:38 +0100

    added custom character and custom shooter character movement

---
+++ Source/ShooterGame/Public/Player/My_ShooterCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/ShooterCharacter.h"
+#include "My_ShooterCharacter.generated.h"
+
+class UMy_ShooterCharacterMovement;
+
+
+UCLASS()
+class SHOOTERGAME_API AMy_ShooterCharacter : public AShooterCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	// With this constructor Unreal will automatically initialize this Character with my new Movement Component
+	AMy_ShooterCharacter(const class FObjectInitializer& ObjectInitializer);
+
+	// Function for get our Movement Component without casting
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	UMy_ShooterCharacterMovement* GetMyMovementComponent() const;
+
+private:
+	// Input binding is set in the character in the original project, overriding this method let us expand those bindings
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Function that will call the Teleport implementation in the Movement Component
+	void Teleport();
+};
+
+
