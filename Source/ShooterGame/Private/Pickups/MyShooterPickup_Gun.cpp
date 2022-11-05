413f629db05caa56d9bbad18ce6d0f78af18aa66
salvatore gurzi <s.gurzi@reply.it>
2022-11-05 16:30:35 +0100

    added new script and blueprint for the dropped weapon feature

---
+++ Source/ShooterGame/Private/Pickups/MyShooterPickup_Gun.cpp
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterGame.h"
+#include "Pickups/MyShooterPickup_Gun.h"
+#include "OnlineSubsystemUtils.h"
+
+/* The MaskMesh object is replicated setting to "Replicate" in the property, using the method "GetLifetimeReplicatedProps" and ensuring that the
+constructor sets bReplicates to true*/
+
+AMyShooterPickup_Gun::AMyShooterPickup_Gun(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	AmmoClips = 0;
+	AmmoLoadedClip = 0;
+
+	PickupMesh = ObjectInitializer.CreateDefaultSubobject<USkeletalMeshComponent>(this, TEXT("PickupMesh"));
+	PickupMesh->SetupAttachment(RootComponent);
+	bReplicates = true;
+}
+
+
+void AMyShooterPickup_Gun::OnRep_MaskMesh() 
+{
+	SetWeaponPickupMesh(MaskMesh);
+}
+
+void AMyShooterPickup_Gun::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Tells the network when to replicate the variables
+	DOREPLIFETIME(AMyShooterPickup_Gun, MaskMesh);  
+}
+
+void AMyShooterPickup_Gun::SetAmmoClips(int32 Clips) 
+{
+	AmmoClips = Clips;
+}
+
+void AMyShooterPickup_Gun::SetAmmoLoadedClip(int32 LoadedClips) 
+{
+	AmmoLoadedClip = LoadedClips;
+}
+void AMyShooterPickup_Gun::SetWeaponType(TSubclassOf<AShooterWeapon> Type) 
+{
+	WeaponType = Type;
+}
+void AMyShooterPickup_Gun::SetWeaponPickupMesh(USkeletalMesh* WeaponMesh) 
+{
+	PickupMesh->SetSkeletalMesh(WeaponMesh, false);
+}
+
+
+void AMyShooterPickup_Gun::GivePickupTo(class AShooterCharacter* Pawn)
+{
+	AShooterWeapon* Weapon = (Pawn ? Pawn->FindWeapon(WeaponType) : NULL);
+	if (Weapon)
+	{
+		int32 Qty = AmmoClips * Weapon->GetAmmoPerClip() + AmmoLoadedClip;
+		Weapon->GiveAmmo(Qty);
+
+		// Fire event for collected ammo
+		if (Pawn)
+		{
+			const UWorld* World = GetWorld();
+			const IOnlineEventsPtr Events = Online::GetEventsInterface(World);
+			const IOnlineIdentityPtr Identity = Online::GetIdentityInterface(World);
+
+			if (Events.IsValid() && Identity.IsValid())
+			{
+				AShooterPlayerController* PC = Cast<AShooterPlayerController>(Pawn->Controller);
+				if (PC)
+				{
+					ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(PC->Player);
+
+					if (LocalPlayer)
+					{
+						const int32 UserIndex = LocalPlayer->GetControllerId();
+						TSharedPtr<const FUniqueNetId> UniqueID = Identity->GetUniquePlayerId(UserIndex);
+						if (UniqueID.IsValid())
+						{
+							FVector Location = Pawn->GetActorLocation();
+
+							FOnlineEventParms Params;
+
+							Params.Add(TEXT("SectionId"), FVariantData((int32)0)); // unused
+							Params.Add(TEXT("GameplayModeId"), FVariantData((int32)1)); // @todo determine game mode (ffa v tdm)
+							Params.Add(TEXT("DifficultyLevelId"), FVariantData((int32)0)); // unused
+
+							Params.Add(TEXT("ItemId"), FVariantData((int32)Weapon->GetAmmoType() + 1)); // @todo come up with a better way to determine item id, currently health is 0 and ammo counts from 1
+							Params.Add(TEXT("AcquisitionMethodId"), FVariantData((int32)0)); // unused
+							Params.Add(TEXT("LocationX"), FVariantData(Location.X));
+							Params.Add(TEXT("LocationY"), FVariantData(Location.Y));
+							Params.Add(TEXT("LocationZ"), FVariantData(Location.Z));
+							Params.Add(TEXT("ItemQty"), FVariantData((int32)Qty));
+
+							Events->TriggerEvent(*UniqueID, TEXT("CollectPowerup"), Params);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+void AMyShooterPickup_Gun::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetLifeSpan(LifeTime);
+}
+
+void AMyShooterPickup_Gun::OnPickedUp()
+{
+	Super::OnPickedUp();
+
+	Destroy();
+}
---
+++ Source/ShooterGame/Public/Pickups/MyShooterPickup_Gun.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickups/ShooterPickup_Ammo.h"
+#include "Weapons/ShooterWeapon.h"
+#include "Net/UnrealNetwork.h"
+#include "MyShooterPickup_Gun.generated.h"
+
+/**
+ *
+ */
+
+ //Class that stores the data of the pickup weapon 
+UCLASS(Abstract, Blueprintable)
+class SHOOTERGAME_API AMyShooterPickup_Gun : public AShooterPickup_Ammo
+{
+	GENERATED_UCLASS_BODY()
+
+
+public:
+
+	//Enable replication of mesh change also 
+	UPROPERTY(ReplicatedUsing = OnRep_MaskMesh)
+	USkeletalMesh* MaskMesh;
+
+	//Replicates mesh changes on clients
+	UFUNCTION()
+	void OnRep_MaskMesh();
+
+	/** Set how many clips this pickup holds */
+	void SetAmmoClips(int32 Clips);
+
+	/** Set how many loaded clips this pickup holds */
+	void SetAmmoLoadedClip(int32 LoadedClips);
+
+	/** Set the weapon type */
+	void SetWeaponType(TSubclassOf<AShooterWeapon> Type);
+
+	/** Set the weapon mesh */
+	void SetWeaponPickupMesh(USkeletalMesh* WeaponMesh);
+
+private:
+
+	/** Mesh component */
+	//TODO: what about using replicate?
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	USkeletalMeshComponent* PickupMesh;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	virtual void OnPickedUp() override;
+
+	virtual void GivePickupTo(AShooterCharacter* Pawn) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+
+	/** How much ammo there are in the currently loaded clip? */
+	UPROPERTY(EditDefaultsOnly, Category = Pickup)
+	int32 AmmoLoadedClip;
+
+	/** how much this pickup lives? */
+	UPROPERTY(EditDefaultsOnly, Category = Pickup)
+	float LifeTime = 10.0f;
+
+	/** Returns Pickup Mesh Component subobject **/
+	FORCEINLINE USkeletalMeshComponent* GetPickupMesh() const { return PickupMesh; }
+
+
+};
